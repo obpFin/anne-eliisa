0d619ab17134e1b83ff0ec11331390318e1ce756
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-07-21 19:03:09 +0300

    changed index page to support collections

--- src/components/image.js
+++ src/components/image.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
 /*
@@ -13,20 +12,9 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const Image = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
+const Image = ({fluid}) => (
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
-}
+  <Img fluid={fluid} />
+)
 
 export default Image
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
-import { Link, useStaticQuery } from "gatsby"
+import { useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Collection from "../components/collection/collection"
 
 const IndexPage = () => {
   const data = useStaticQuery(
@@ -22,16 +22,12 @@ const IndexPage = () => {
       }
     `
   )
-  const asset = data.allContentfulAsset.edges[0].node
-  console.log(asset.fluid)
-
 
   return (
     <Layout>
       <SEO title="Home" />
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <h2>{asset.title}</h2>
-        <Image fluid={asset.fluid} alt={asset.title} />
+      <div style={{ marginBottom: `1.45rem` }}>
+        <Collection />
       </div>
     </Layout>
   )
